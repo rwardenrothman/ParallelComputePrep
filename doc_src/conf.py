8eecd5d547b1652333ff1f4719eb19ded70a0eee
rwarden <rwarden@alum.mit.edu>
2025-06-16 12:14:13 -0400

    Initial sphinx commit

---
+++ doc_src/conf.py
@@ -0,0 +1,59 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Takeda Parallel Compute Prep'
+copyright = '2025, Rob Warden-Rothman'
+author = 'Rob Warden-Rothman'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.napoleon',
+    'sphinx.ext.githubpages',
+    'myst_parser',
+    'sphinx.ext.autosectionlabel',
+    'sphinxcontrib.plantuml',
+    'nbsphinx',
+]
+
+source_suffix = ['.rst', '.md']
+
+templates_path = ['_templates']
+exclude_patterns = []
+autosectionlabel_maxdepth = 3
+autosectionlabel_prefix_document = True
+
+myst_url_schemes = {
+    "http": None,
+    "https": None,
+    "tel": {
+        "url": "tel:{{path}}",
+        "title": "{{path}}",
+    },
+    "mailto": {
+        "url": "mailto:{{path}}",
+        "title": "{{path}}",
+    },
+}
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_book_theme'
+html_static_path = ['_static']
+html_theme_options = {
+    'body_max_width': 'none'
+}
+
+plantuml = r'java -jar "C:\Users\rward\AppData\Local\JetBrains\PyCharm2024.3\markdown\download\plantumllanguageextension\plantuml.jar"'
+plantuml_output_format = 'svg'
+
+import os
+os.environ['PATH'] += f';{os.environ["PANDOC_PATH"]}'
---
+++ doc_src/index.rst
@@ -0,0 +1,17 @@
+.. Takeda Parallel Compute Prep documentation master file, created by
+   sphinx-quickstart on Mon Jun 16 12:05:23 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Takeda Parallel Compute Prep documentation
+==========================================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
